518993bb1613f9c3fcbf7fe49d2f00772de37609
davefranklin02 <36048455+davefranklin02@users.noreply.github.com>
2021-01-17 11:44:41 -0500

    Add files via upload

---
+++ del.sh
@@ -0,0 +1,4 @@
+kubectl delete deployment db
+kubectl delete deployment mvccoredocker
+kubectl delete svc db
+kubectl delete svc mvccoredocker
---
+++ deploy.sh
@@ -0,0 +1,4 @@
+kubectl apply -f dd.yaml
+sleep 30
+kubectl apply -f d.yaml
+
